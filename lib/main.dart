d9c03305fdb21d285f45a1e3d7e50c22edd23fd5
M HAMZA KHAN <157797338+hk994512@users.noreply.github.com>
2024-06-29 10:11:27 +0500

    basic_calculater

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:go_moon_app/screens/calculator_view.dart';
+
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+        cardColor: const Color.fromARGB(255, 9, 16, 54),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.red, onBackground: Colors.white)),
+    home: const Home()));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 9, 16, 54),
+          title: Text(
+            "Calculator",
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+        ),
+        body: const CalculaterView());
+  }
+}
---
+++ lib/screens/calculater_screen.dart.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CalculaterView extends StatefulWidget {
+  const CalculaterView({super.key});
+
+  @override
+  State<CalculaterView> createState() => _CalculaterViewState();
+}
+
+class _CalculaterViewState extends State<CalculaterView> {
+  final _textOne = TextEditingController();
+  final _textTwo = TextEditingController();
+  int x = 0;
+  int y = 0;
+  double z = 0;
+  @override
+  void initState() {
+    _textOne.text = x.toString();
+    _textTwo.text = y.toString();
+    super.initState();
+  }
+
+  double calculateResult(String n) {
+    switch (n) {
+      case "+":
+        z = double.parse(_textOne.text) + double.parse(_textTwo.text);
+        break;
+      case "-":
+        z = double.parse(_textOne.text) - double.parse(_textTwo.text);
+        break;
+      case "*":
+        z = double.parse(_textOne.text) * double.parse(_textTwo.text);
+        break;
+      case "/":
+        z = double.parse(_textOne.text) / double.parse(_textTwo.text);
+        break;
+      case "clear":
+        _textOne.clear();
+        _textTwo.clear();
+        z = 0;
+        break;
+    }
+    return z;
+  }
+
+  @override
+  void dispose() {
+    _textOne.dispose();
+    _textTwo.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Scaffold(
+        body: Column(
+          children: [
+            Calculater(
+              textOne: _textOne,
+              label: "Enter first number",
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Calculater(textOne: _textTwo, label: "Enter second number"),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              z.toString(),
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontSize: 35,
+                  ),
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomButton(
+                  icon: CupertinoIcons.add,
+                  result: () {
+                    setState(() {
+                      calculateResult("+");
+                    });
+                  },
+                ),
+                CustomButton(
+                  icon: CupertinoIcons.minus,
+                  result: () {
+                    setState(() {
+                      calculateResult("-");
+                    });
+                  },
+                ),
+                CustomButton(
+                  icon: CupertinoIcons.multiply,
+                  result: () {
+                    setState(() {
+                      calculateResult("*");
+                    });
+                  },
+                ),
+                CustomButton(
+                  icon: CupertinoIcons.divide,
+                  result: () {
+                    setState(() {
+                      calculateResult("/");
+                    });
+                  },
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.pink),
+                  onPressed: () {
+                    setState(() {
+                      calculateResult("clear");
+                    });
+                  },
+                  child: const Text("Clear"),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.icon, required this.result});
+  final IconData icon;
+  final Function() result;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        style: TextButton.styleFrom(
+            foregroundColor: Colors.white, backgroundColor: Colors.pink),
+        onPressed: result,
+        child: Icon(icon));
+  }
+}
+
+class Calculater extends StatelessWidget {
+  const Calculater({super.key, required this.textOne, required this.label});
+
+  final TextEditingController textOne;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.black, width: 3),
+              borderRadius: BorderRadius.circular(10)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.black, width: 3),
+              borderRadius: BorderRadius.circular(10))),
+      controller: textOne,
+      keyboardType: const TextInputType.numberWithOptions(
+        decimal: true,
+      ),
+    );
+  }
+}
